6381f1b92ac3b62549cd6304a03dd1a24c7581ae
Marcell Dechant <marcell.dechant@proton.me>
2025-11-17 12:49:51 +0100

    BIG BANG

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Title from "./Title.tsx";
+import Footer from "./Footer.tsx";
+
+export default function App() {
+
+    return (
+        <>
+            <h1>Hello REACT!</h1>
+            <Title />
+            <Footer />
+        </>
+    )
+}
---
+++ src/Title.tsx
@@ -0,0 +1,8 @@
+export default function Title() {
+    return (
+        <div>
+            <h1>Hello Website</h1>
+            <h4>Hello subtitle!</h4>
+        </div>
+    );
+}
